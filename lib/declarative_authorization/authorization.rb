e63dcc54d8d466c05ee4e9a7bbf5a014f87e443e
Daniel Vicory <daniel.vicory@appfolio.com>
2017-11-09 17:18:25 -0800

    dtv - cache guest user and add more accessors

--- lib/declarative_authorization/authorization.rb
+++ lib/declarative_authorization/authorization.rb
@@ -25,12 +25,20 @@ module Authorization
   # Controller-independent method for retrieving the current user.
   # Needed for model security where the current controller is not available.
   def self.current_user
-    Thread.current["current_user"] || AnonymousUser.new
+    Thread.current["current_user"] || guest_user
   end
 
   # Controller-independent method for setting the current user.
   def self.current_user=(user)
     Thread.current["current_user"] = user
+  end
+
+  def self.guest_user
+    @@guest_user ||= AnonymousUser.new
+  end
+
+  def self.non_guest_current_user
+    current_user unless current_user.is_a?(AnonymousUser)
   end
 
   # For use in test cases only
