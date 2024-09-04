d18285f0f2ba240e0eff21bea53d4a37fd518342
Michael Lambert <michael.lambert@appfolio.com>
2024-09-04 12:13:53 -0700

    Add create only and edit only privileges

--- lib/declarative_authorization/test/helpers.rb
+++ lib/declarative_authorization/test/helpers.rb
@@ -81,8 +81,8 @@ module DeclarativeAuthorization
         def privilege(privilege, &block)
           privileges = [privilege].flatten.uniq
 
-          unless privileges.all? { |privilege| [:hidden, :read, :write, :write_without_delete].include?(privilege) }
-            raise "Privilege (:when) must be :hidden, :read, :write_without_delete, or :write. Found #{privilege.inspect}."
+          unless privileges.all? { |privilege| [:hidden, :read, :create, :edit, :write, :write_without_delete].include?(privilege) }
+            raise "Privilege (:when) must be :hidden, :read, :create, :edit, :write_without_delete, or :write. Found #{privilege.inspect}."
           end
 
           Blockenspiel.invoke(block, PrivilegeTestGenerator.new(@test_class, @role, privileges))
